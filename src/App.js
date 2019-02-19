8f5031658188fb5369fa44e903feac264cd25eda
CodeXRook <lacekitfly@gmail.com>
2019-02-19 14:22:17 -0500

    updated App imported Axios

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import Axios from 'axios';
+import React, {Component } from 'react';
+import './App.css';
+import Profile from './components/profile';
+import {Homepage} from './components/homepage';
+import Searchbar from './components/searchbar';
+
+
+export default App;
