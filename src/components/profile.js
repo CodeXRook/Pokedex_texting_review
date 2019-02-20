decf6ebc06364cd36e25678f8b8defc603736611
CodeXRook <lacekitfly@gmail.com>
2019-02-20 04:51:53 -0500

    set up profile js

---
+++ src/components/profile.js
@@ -0,0 +1,11 @@
+import Axios from 'axios';
+import React from 'react';
+import './profile.css';
+
+class Profile extends Component {
+    constructor (props) {
+        super(props);
+
+        this
+    }
+}
