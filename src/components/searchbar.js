0d02812a7caf7c6349b48661037fcd4aa4a22af9
CodeXRook <lacekitfly@gmail.com>
2019-02-20 01:52:43 -0500

    searchbar setup

---
+++ src/components/searchbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './searchbar.css';
+import {pkmn_list} from './pkmn-list';
+
+class Searchbar extends React.Component{
+    constructor(props){
+        super(props);
+        this
+    }
+}
